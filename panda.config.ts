274c10c915ae03d391e7f5e3afed1ac4518c1891
mimi <62527898+mimi-0506@users.noreply.github.com>
2025-04-03 22:32:31 +0900

    feat: panda css 글로벌 설정 추가
    * 스크롤바 없앤 걸 default로 줌

--- panda.config.ts
+++ panda.config.ts
@@ -15,6 +15,15 @@ export default defineConfig({
     extend: {},
   },
 
+  globalCss: {
+    "*, *::before, *::after": {
+      scrollbarWidth: "none", // Firefox에서 스크롤 숨기기
+      msOverflowStyle: "none", // IE, Edge에서 스크롤 숨기기
+    },
+    "*::-webkit-scrollbar": {
+      display: "none", // Chrome, Safari에서 스크롤 숨기기
+    },
+  },
   jsxFramework: "react",
 
   // The output directory for your css system
